e0be0b2b0f7f36a746f91afa35c4142313b9923c
Nix <anikeev@webunix.ru>
2018-10-15 13:34:36 +0400

    upd

--- bot.go
+++ bot.go
@@ -17,11 +17,11 @@ import (
 
 const (
 	//Password type mode autorization on enter password
-	Password   = 1
+	password = 1
 	//Key type mode autorization on public_key
-	Key        = 2
+	key = 2
 	//DefTimeout timeout by default
-	DefTimeout = 3 // second
+	defTimeout = 3 // second
 )
 
 //Config структура для Json "Config"
@@ -33,9 +33,10 @@ type config struct {
 		Cert  string `json:"cert"`
 		Token string `json:"token"`
 		Proxy string `json:"proxy"`
-		Mode int `json:"mode"`
+		Mode  int    `json:"mode"`
 	} `json:"ssh"`
 }
+
 //SSH struct for config
 type shell struct {
 	IP      string
@@ -52,83 +53,97 @@ var conf config
 func readConfing() {
 
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	fuck("Error pwd:", err)
-
+	if err != nil {
+		log.Printf("Error pwd:%v\n", err)
+	}
 	configFile, err := ioutil.ReadFile(dir + "/config.json")
 	if err != nil {
 		panic(err)
 	}
 	err = json.Unmarshal(configFile, &conf)
-	fuck("Ошибка Unmarshal", err)
-
+	if err != nil {
+		log.Printf("Error Unmarshal%v\n", err)
+	}
 }
 func (sshClient *shell) readPublicKeyFile(file string) ssh.AuthMethod {
 	buffer, err := ioutil.ReadFile(file)
 	if err != nil {
 		return nil
 	}
-
 	key, err := ssh.ParsePrivateKey(buffer)
 	if err != nil {
 		return nil
 	}
 	return ssh.PublicKeys(key)
 }
+
 //Connect - create connecting session of ssh
 func (sshClient *shell) connect(mode int) {
 
 	var sshConfig *ssh.ClientConfig
 	var auth []ssh.AuthMethod
-	if mode == Password {
+	if mode == password {
 		auth = []ssh.AuthMethod{ssh.Password(sshClient.Cert)}
-	} else if mode == Key {
+	} else if mode == key {
 		auth = []ssh.AuthMethod{sshClient.readPublicKeyFile(sshClient.Cert)}
 	} else {
 		log.Println("does not support mode: ", mode)
+
 		return
 	}
 
 	sshConfig = &ssh.ClientConfig{
 		User: sshClient.User,
 		Auth: auth,
-
 		HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
 			return nil
 		},
-		Timeout: time.Second * DefTimeout,
+		Timeout: time.Second * defTimeout,
 	}
 
 	client, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", sshClient.IP, sshClient.Port), sshConfig)
 	if err != nil {
 		fmt.Println(err)
+
 		return
 	}
-
 	session, err := client.NewSession()
 	if err != nil {
-		log.Printf("Error of NewSession %v\n",err)
-		err:=client.Close()
-		fuck("Error of close client",err)
+		log.Printf("Error of NewSession %v\n", err)
+		err := client.Close()
+		if err != nil {
+			log.Printf("Error of close client NewSession%v\n", err)
+		}
+
 		return
 	}
 
 	sshClient.session = session
 	sshClient.client = client
 }
+
 //RunCmd do command line
 func (sshClient *shell) RunCmd(cmd string) string {
 	sshClient.connect(conf.SSH.Mode)
 	out, err := sshClient.session.CombinedOutput(cmd)
-	fuck("Error of RunCmd", err)
+	if err != nil {
+		log.Printf("Error of RunCmd %v\n", err)
+	}
 	sshClient.Close()
+
 	return string(out)
 }
+
 //Close close session of ssh
 func (sshClient *shell) Close() {
 	err := sshClient.session.Close()
-	fuck("Error of close session",err)
+	if err != nil {
+		log.Printf("Error of close session %v\n", err)
+	}
 	err = sshClient.client.Close()
-	fuck("Error of close client",err)
+	if err != nil {
+		log.Printf("Error of close client %v\n", err)
+	}
 }
 
 //demo
@@ -144,17 +159,14 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
-
 	bot.Debug = true
-
 	log.Printf("Authorized on account %s", bot.Self.UserName)
-
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
-
 	updates, err := bot.GetUpdatesChan(u)
-	fuck("Error of GetUpdatesChan",err)
-
+	if err != nil {
+		log.Printf("Error of GetUpdatesChan %v\n", err)
+	}
 	//client.Connect(Key)
 	for update := range updates {
 		if update.Message == nil {
@@ -165,16 +177,10 @@ func main() {
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 			ms := client.RunCmd(s)
 			msg.Text = ms
-			_,err:=bot.Send(msg)
-			fuck("Error of send Bot",err)
-
+			_, err := bot.Send(msg)
+			if err != nil {
+				log.Printf("Error of send Bot %v\n", err)
+			}
 		}
-
-	}
-	//client.Close()
-}
-func fuck(s string,e error){
-	if e != nil {
-		log.Printf(s+" %v\n",e)
 	}
 }
